0d17ee3f09e4039e90521dfcc627354b125a0264
Ryan Merritt <merrittaudio@gmail.com>
2022-08-19 14:25:40 -0500

    file-to-buffer and buffer-to-file

--- src/idi.c
+++ src/idi.c
@@ -1,9 +1,62 @@
 #include "idi.h"
 
+static size
+GetFileSize(FILE *File)
+{
+    size Result;
+
+    fseek(File, 0, SEEK_END);
+    Result = ftell(File);
+
+    return(Result);
+}
+
+static buffer *
+CreateBuffer(size Size)
+{
+    buffer *Result = malloc(sizeof(buffer));
+    Result->Size = Size;
+    u8 *Data = malloc(Size * sizeof(u8));
+    Result->Data = Data;
+
+    return Result;
+}
+
+static buffer *
+ReadFileIntoBuffer(const char *FilePath)
+{
+    FILE *File = fopen(FilePath, "rb");
+    size FileSize = GetFileSize(File);
+    buffer *Result = CreateBuffer(FileSize);
+
+    fseek(File, 0, SEEK_SET);
+    fread(Result->Data, sizeof(*Result->Data), FileSize, File);
+    fclose(File);
+
+    return Result;
+}
+
+static void
+WriteBufferToFile(buffer *Buffer, const char *FilePath)
+{
+    FILE *File = fopen(FilePath, "wb");
+
+    fwrite(Buffer->Data, sizeof(*Buffer->Data), Buffer->Size, File);
+    fclose(File);
+}
+
 int
 main()
 {
-    int Result = 42;
+    int Result = 0;
+    buffer *Buffer = ReadFileIntoBuffer("idi.idi");
+
+    WriteBufferToFile(Buffer, "test.idi");
+
+    Result = Buffer->Size;
+
+    free(Buffer->Data);
+    free(Buffer);
 
     return Result;
 }
--- src/idi.h
+++ src/idi.h
@@ -1,6 +1,17 @@
 #ifndef IDI_H_
 #define IDI_H_
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
 
+typedef uint8_t u8;
+typedef size_t size;
+
+struct buffer {
+    size Size;
+    u8 *Data;
+};
+typedef struct buffer buffer;
 
 #endif // IDI_H_
