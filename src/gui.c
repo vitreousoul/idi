d4267c3d9666619394d09350c3b7ce4286a44704
Ryan Merritt <merrittaudio@gmail.com>
2022-11-25 17:40:31 -0600

    use glyph metrics for spacing

--- src/gui.c
+++ src/gui.c
@@ -1,15 +1,21 @@
 #define SCREEN_WIDTH 480
 #define SCREEN_HEIGHT 320
 
-#define MAX_KEY_CODE_BUFFER 1 << 12
-u32 KEY_CODE_BUFFER[MAX_KEY_CODE_BUFFER];
+#define MAX_KEY_CODE_CACHE 1 << 12
+u32 KEY_CODE_CACHE[MAX_KEY_CODE_CACHE];
 
 #define MIN_KEY_CODE 20
 #define MAX_KEY_CODE 128
-#define MAX_TEXTURE_BUFFER (MAX_KEY_CODE - MIN_KEY_CODE)
-SDL_Texture *TEXTURE_BUFFER[MAX_TEXTURE_BUFFER];
 
-char *FONT_PATH = "src/PTMono-Regular.ttf";
+#define MAX_TEXTURE_CACHE_SIZE (MAX_KEY_CODE - MIN_KEY_CODE)
+SDL_Texture *TEXTURE_CACHE[MAX_TEXTURE_CACHE_SIZE];
+gui_glyph_metric GLYPH_METRIC_CACHE[MAX_TEXTURE_CACHE_SIZE];
+
+char *FONT_PATH[] = {
+    "src/PTMono-Regular.ttf",
+    "src/ZapfDingbats.ttf",
+    "src/Bodoni Ornaments.ttf",
+};
 
 static result Init()
 {
@@ -32,20 +38,37 @@ static result Init()
     return(Result);
 }
 
-static void InitTextureCache(SDL_Renderer *Renderer)
+static void InitTextureCache(SDL_Renderer *Renderer, gui_state State)
 {
     u32 I;
-    TTF_Font *Font = TTF_OpenFont(FONT_PATH, 20);
+    TTF_Font *Font = TTF_OpenFont(FONT_PATH[0], State.FontSize);
     if(Font == 0) PrintError("TTF_OpenFont");
     SDL_Color FontColorFG = {220,200,0,255};
     SDL_Color FontColorBG = {0,0,0,255};
 
     for (I = MIN_KEY_CODE; I < MAX_KEY_CODE; I++)
     {
+        u32 CacheIndex = I - MIN_KEY_CODE;
+        s32 MinX = 0;
+        s32 MaxX = 0;
+        s32 MinY = 0;
+        s32 MaxY = 0;
+        s32 Advance = 0;
         assert(Font);
         SDL_Surface *Surface = TTF_RenderGlyph_Shaded(Font, I, FontColorFG, FontColorBG);
+        /* SDL_Surface *Surface = TTF_RenderGlyph_Solid(Font, I, FontColorFG); */
+
+
         assert(Surface);
-        TEXTURE_BUFFER[I - MIN_KEY_CODE] = SDL_CreateTextureFromSurface(Renderer, Surface);
+        TEXTURE_CACHE[CacheIndex] = SDL_CreateTextureFromSurface(Renderer, Surface);
+
+        TTF_GlyphMetrics(Font, I, &MinX, &MaxX, &MinY, &MaxY, &Advance);
+        GLYPH_METRIC_CACHE[CacheIndex].MinX = MinX;
+        GLYPH_METRIC_CACHE[CacheIndex].MaxX = MaxX;
+        GLYPH_METRIC_CACHE[CacheIndex].MinY = MinY;
+        GLYPH_METRIC_CACHE[CacheIndex].MaxY = MaxY;
+        GLYPH_METRIC_CACHE[CacheIndex].Advance = Advance;
+
         SDL_FreeSurface(Surface);
     }
 }
@@ -54,9 +77,9 @@ static void DeInit(SDL_Window *Window, SDL_Renderer *Renderer)
 {
     printf("DeInit\n");
     u32 I;
-    for (I = 0; I < MAX_TEXTURE_BUFFER; I++)
+    for (I = 0; I < MAX_TEXTURE_CACHE_SIZE; I++)
     {
-        SDL_DestroyTexture(TEXTURE_BUFFER[I]);
+        SDL_DestroyTexture(TEXTURE_CACHE[I]);
     }
 
     SDL_DestroyWindow(Window);
@@ -77,6 +100,73 @@ static SDL_Rect CreateRect(u32 x, u32 y, u32 w, u32 h)
     return(Result);
 }
 
+static gui_state InitGuiState()
+{
+    gui_state Result;
+    Result.Running = 1;
+    Result.FontSize = 36;
+    Result.Cursor.BufferIndex = 0;
+    Result.Cursor.X = 0;
+    Result.Cursor.Y = 0;
+
+    return(Result);
+}
+
+static b32 HandleEvents(gui_state *State)
+{
+    SDL_Event Event;
+    b32 HadKeyboardEvent = 0;
+
+    while(SDL_PollEvent(&Event))
+    {
+        switch(Event.type)
+        {
+        case SDL_KEYDOWN:
+        {
+            HadKeyboardEvent = 1;
+
+            if(Event.key.keysym.mod)
+            {
+                switch(Event.key.keysym.mod)
+                {
+                case KMOD_LCTRL:
+                {
+                    switch(Event.key.keysym.sym)
+                    {
+                    case SDLK_MINUS:
+                    {
+
+                    } break;
+                    case SDLK_EQUALS:
+                    {
+
+                    } break;
+                    }
+                } break;
+                }
+
+            }
+            else if(Event.key.keysym.sym >= MIN_KEY_CODE && Event.key.keysym.sym <= MAX_KEY_CODE)
+            {
+                KEY_CODE_CACHE[State->Cursor.BufferIndex] = Event.key.keysym.sym - MIN_KEY_CODE;
+                ++State->Cursor.BufferIndex;
+                if(State->Cursor.BufferIndex >= MAX_KEY_CODE_CACHE)
+                {
+                    PrintError("key code buffer overflow");
+                    State->Running = 0;
+                }
+            }
+        } break;
+        case SDL_QUIT:
+        {
+            State->Running = 0;
+        } break;
+        }
+    }
+
+    return HadKeyboardEvent;
+}
+
 void DisplayWindow()
 {
     printf("DisplayWindow\n");
@@ -84,6 +174,7 @@ void DisplayWindow()
 
     if(InitResult == result_Error)
     {
+        PrintError("Init error");
         return;
     }
 
@@ -93,50 +184,18 @@ void DisplayWindow()
                                           SCREEN_WIDTH, SCREEN_HEIGHT,
                                           SDL_WINDOW_HIDDEN);
     SDL_Renderer *Renderer = SDL_CreateRenderer(Window, -1, 0);
-
     assert(Renderer);
-    InitTextureCache(Renderer);
 
-    cursor Cursor;
-    Cursor.BufferIndex = 0;
-    Cursor.X = 0;
-    Cursor.Y = 0;
+    gui_state State = InitGuiState();
+    InitTextureCache(Renderer, State);
 
     SDL_ShowWindow(Window);
 
-    SDL_Event Event;
-    u32 Running = 1;
     u32 DelayInMilliseconds = 32;
 
-    while(Running)
+    while(State.Running)
     {
-        b32 HadKeyboardEvent = 0;
-
-        while(SDL_PollEvent(&Event))
-        {
-            switch(Event.type)
-            {
-            case SDL_KEYDOWN:
-            {
-                HadKeyboardEvent = 1;
-
-                if(Event.key.keysym.sym >= MIN_KEY_CODE && Event.key.keysym.sym <= MAX_KEY_CODE)
-                {
-                    KEY_CODE_BUFFER[Cursor.BufferIndex] = Event.key.keysym.sym - MIN_KEY_CODE;
-                    ++Cursor.BufferIndex;
-                    if(Cursor.BufferIndex >= MAX_KEY_CODE_BUFFER)
-                    {
-                        PrintError("key code buffer overflow");
-                        Running = 0;
-                    }
-                }
-            } break;
-            case SDL_QUIT:
-            {
-                Running = 0;
-            } break;
-            }
-        }
+        b32 HadKeyboardEvent = HandleEvents(&State);
 
         if(HadKeyboardEvent)
         {
@@ -144,19 +203,24 @@ void DisplayWindow()
 
 #if 1
             {
-                u32 ScaleX = 12;
-                u32 ScaleY = 20;
                 DEBUG_Rect.x = 0;
                 DEBUG_Rect.y = 0;
                 u32 I;
 
-                for(I = 0; I < Cursor.BufferIndex; ++I)
+                for(I = 0; I < State.Cursor.BufferIndex; ++I)
                 {
-                    DEBUG_Rect.x = (I * ScaleX) % SCREEN_WIDTH;
-                    DEBUG_Rect.y = ((I * ScaleX) / SCREEN_WIDTH) * ScaleY;
-                    SDL_Texture *Texture = TEXTURE_BUFFER[KEY_CODE_BUFFER[I]];
+                    u32 KeyCodeIndex = KEY_CODE_CACHE[I];
+                    gui_glyph_metric GlyphMetric = GLYPH_METRIC_CACHE[KeyCodeIndex];
+                    SDL_Texture *Texture = TEXTURE_CACHE[KeyCodeIndex];
+
+                    if((DEBUG_Rect.x + GlyphMetric.Advance) > SCREEN_WIDTH)
+                    {
+                        DEBUG_Rect.x = 0;
+                        DEBUG_Rect.y += 40;
+                    }
+
                     SDL_RenderCopy(Renderer, Texture, NULL, &DEBUG_Rect);
-                    DEBUG_Rect.x += ScaleX;
+                    DEBUG_Rect.x += GlyphMetric.Advance;
                 }
             }
 #endif
--- src/gui.h
+++ src/gui.h
@@ -1,8 +1,24 @@
-typedef struct
+typedef struct gui_cursor
 {
     u32 BufferIndex;
     u32 X;
     u32 Y;
-} cursor;
+} gui_cursor;
+
+typedef struct gui_state
+{
+    u32 Running;
+    u32 FontSize;
+    gui_cursor Cursor;
+} gui_state;
+
+typedef struct gui_glyph_metric
+{
+    s32 MinX;
+    s32 MaxX;
+    s32 MinY;
+    s32 MaxY;
+    s32 Advance;
+} gui_glyph_metric;
 
 void DisplayWindow(void);
