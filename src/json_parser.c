8862abf54647639000ef86436b40b3a12c6ab5d7
Ryan Merritt <merrittaudio@gmail.com>
2022-12-22 21:00:47 -0600

    better numbers, broken object

--- src/json_parser.c
+++ src/json_parser.c
@@ -5,6 +5,7 @@
 json_token Tokens[MAX_JSON_TOKEN_CHUNK_COUNT];
 
 static void ParserError(json_parser *Parser, char *Message)
+
 {
     PrintError(Message);
     Parser->State = json_parser_state_Error;
@@ -139,8 +140,8 @@ static json_buffer_range ParseNumber(buffer *Buffer, json_parser *Parser)
             break;
         }
     }
-
-    Result.End = Parser->Index - 1;
+    Result.End = Parser->Index;
+    /* Parser->State = json_parser_state_Success; */
 
     return(Result);
 }
@@ -218,13 +219,13 @@ static u32 ParseJsonBuffer(buffer *Buffer)
             json_token_type TokenType = json_token_type_Empty;
             json_buffer_range TokenRange = {0,0};
 
-            if(CharIsDigit(Buffer->Data[Parser.Index]))
+            if(CharIsDigit(Buffer->Data[Parser.Index]) || Buffer->Data[Parser.Index] == '.')
             {
                 TokenRange = ParseNumber(Buffer, &Parser);
-
                 if(TokenRange.End > TokenRange.Start)
                 {
                     TokenType = json_token_type_Number;
+                    Tokens[TokenIndex].Range = TokenRange;
                 }
             }
             else if(CharIsStartOfBoolean(Buffer->Data[Parser.Index]))
@@ -276,21 +277,28 @@ static json_value *ParseJsonTokens(json_parser *Parser, u32 TokenCount)
             Result->Type = json_value_Boolean;
             Result->Value.Boolean = True;
             ++Parser->Index;
-            Parser->State = json_parser_state_Error;
+            Parser->State = json_parser_state_Success;
         } break;
         case json_token_type_False:
         {
             Result->Type = json_value_Boolean;
             Result->Value.Boolean = False;
             ++Parser->Index;
-            Parser->State = json_parser_state_Error;
+            Parser->State = json_parser_state_Success;
         } break;
         case json_token_type_String:
         {
             Result->Type = json_value_String;
             Result->Value.Range = Tokens[Parser->Index].Range;
             ++Parser->Index;
-            Parser->State = json_parser_state_Error;
+            Parser->State = json_parser_state_Success;
+        } break;
+        case json_token_type_Number:
+        {
+            Result->Type = json_value_Number;
+            Result->Value.Range = Tokens[Parser->Index].Range;
+            ++Parser->Index;
+            Parser->State = json_parser_state_Success;
         } break;
         case json_token_type_OpenCurly:
         {
@@ -366,16 +374,17 @@ static json_value *ParseJsonTokens(json_parser *Parser, u32 TokenCount)
         {
             Result->Type = json_value_Array;
             json_array *CurrentItem = malloc(sizeof(json_array));
-            json_array *FirstItem = CurrentItem;
+            CurrentItem->Value = 0;
+            CurrentItem->Next = 0;
+            Result->Value.Array = CurrentItem;
             ++Parser->Index;
 
             while(Parser->State == json_parser_state_Running && ValidJsonToken(Tokens[Parser->Index]))
             {
                 if(Tokens[Parser->Index].Type == json_token_type_CloseSquare)
                 {
-                    Parser->State = json_parser_state_Error;
+                    Parser->State = json_parser_state_Success;
                     ++Parser->Index;
-                    Result->Value.Array = FirstItem;
                 }
                 else
                 {
@@ -424,12 +433,21 @@ static void PrintJsonValue(buffer *Buffer, json_value *Value, u32 Depth)
     case json_value_String:
     {
         u32 CharCount = Value->Value.Range.End - Value->Value.Range.Start;
-        u8 StringValue[CharCount];
+        u8 StringValue[CharCount + 1];
+        StringValue[CharCount] = NULL_CHAR;
         memcpy(StringValue, Buffer->Data + Value->Value.Range.Start, CharCount);
         printf("String %s\n", StringValue);
     } break;
-    case json_value_Number: { printf("Number\n"); } break;
-    case json_value_Boolean: { printf("Boolean\n"); } break;
+    case json_value_Number: {
+        u32 CharCount = Value->Value.Range.End - Value->Value.Range.Start;
+        u8 StringValue[CharCount + 1];
+        StringValue[CharCount] = NULL_CHAR;
+        memcpy(StringValue, Buffer->Data + Value->Value.Range.Start, CharCount);
+        printf("Number %s\n", StringValue);
+    } break;
+    case json_value_Boolean: {
+        printf("Boolean %d\n", Value->Value.Boolean);
+    } break;
     case json_value_Object:
     {
         json_object *Object = Value->Value.Object;
