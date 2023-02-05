ecf4c8e0fb4a7d8ffe8f11f8aa20fd06c4581787
Ryan Merritt <merrittaudio@gmail.com>
2023-02-05 17:55:26 -0600

    identifier can start with upper-case

--- src/js_lex.c
+++ src/js_lex.c
@@ -159,6 +159,11 @@ static token ParseToken(lexer *Lexer)
         case 'k': case 'l': case 'm': case 'n': case 'o':
         case 'p': case 'q': case 'r': case 's': case 't':
         case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
+        case 'A': case 'B': case 'C': case 'D': case 'E':
+        case 'F': case 'G': case 'H': case 'I': case 'J':
+        case 'K': case 'L': case 'M': case 'N': case 'O':
+        case 'P': case 'Q': case 'R': case 'S': case 'T':
+        case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
             Token = ScanIdentifier(Lexer);
             Running = 0;
             break;
