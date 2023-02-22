20b5ce95548971a01bc55a16789e3259942eb644
Ryan Merritt <rmerritt@nextpoint.com>
2023-02-22 13:23:47 -0600

    module grammar update

--- src/js_parse.c
+++ src/js_parse.c
@@ -2,17 +2,25 @@
 //   Grammar
 // ==========
 //
-// TopLevel = Statement*
+// NOTE: we are starting off with module declarations so we don't have to tackle the entire
+// grammar at once. Also, modules are usually at the top of the file, so we can parse the
+// beginning of some set of files and display dependencies.
 //
-// Statement = StatementContent /;/
-// StatementContent = Assignment | Module
-// Declaration = Class | Function | NameSpace
-//
+// TopLevel = Declaration*
+// Identifier = /[_a-zA-Z]/ /[_a-zA-Z0-9]/*
+// String = ...
+// Declaration = Module
 // Module = Import | Export
-//
-// Import = /import/ ImportType ImportPath
-// ImportType = ImportType /from/
-// ImportPath = String
+// Import = /import/ ModuleType? ModulePath /;/
+// Export = /export/ ModuleType? ModulePath /;/
+// ModuleType = ModuleParams /from/
+// ModuleParams = Identifier | ModuleParamObject
+// ModulePath = String
+// ModuleParamObject = /{/ ModuleParam* /}/
+// ModuleParamAttribute = ModuleParam ModuleParamAttributeNext*
+// ModuleParamAttributeNext = /,/ ModuleParam
+// ModuleParam = Identifier ModuleParamAlias?
+// ModuleParamAlias = /as/ Identifier
 //
 
 static void ParseJs(buffer Source, token *Tokens, u32 TokenCount)
